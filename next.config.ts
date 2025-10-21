c6f2115f3a11bdad94f83118e4a3f2d5b3dec434
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2025-10-22 01:02:13 +0200

    u

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
   images: {
     unoptimized: false,
   },
